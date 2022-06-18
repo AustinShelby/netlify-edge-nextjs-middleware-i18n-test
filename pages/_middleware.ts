4ff76c37b91a88d667e0e5840b47c4f5e1ed9e79
Austin Shelby <helloaustinshelby@gmail.com>
2022-06-18 16:43:50 +0200

    logs

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,17 +3,24 @@ import { NextRequest, NextResponse } from "next/server";
 const PUBLIC_FILE = /\.(.*)$/;
 
 export function middleware(request: NextRequest) {
+  console.log(request.nextUrl);
+  // console.log(request.nextUrl.);
   const shouldHandleLocale =
     !PUBLIC_FILE.test(request.nextUrl.pathname) &&
     !request.nextUrl.pathname.includes("/api/") &&
-    (request.nextUrl.locale === "default" || request.nextUrl.locale === "");
+    !request.nextUrl.pathname.startsWith("/en") &&
+    !request.nextUrl.pathname.startsWith("/de");
+  // (request.nextUrl.locale === "default" || request.nextUrl.locale === "");
 
   console.log(
     `Running middleware.\nLocale: ${request.nextUrl.locale}.\nUrl: ${request.nextUrl.pathname}.\nShould handle: ${shouldHandleLocale}`
   );
   if (shouldHandleLocale) {
     const url = request.nextUrl.clone();
-    url.pathname = `/de${request.nextUrl.pathname}`;
+    const mm = `/de${request.nextUrl.pathname}`;
+    console.log(mm);
+    // url.pathname = mm;
+    url.locale = "de";
     return NextResponse.redirect(url);
   }
 
