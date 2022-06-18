5a3df0a4d640735574cf66a3db87a9456088e004
Austin Shelby <helloaustinshelby@gmail.com>
2022-06-18 16:29:04 +0200

    more logs

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,12 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 const PUBLIC_FILE = /\.(.*)$/;
 
 export function middleware(request: NextRequest) {
-  console.log("Running middleware!");
   const shouldHandleLocale =
     !PUBLIC_FILE.test(request.nextUrl.pathname) &&
     !request.nextUrl.pathname.includes("/api/") &&
     request.nextUrl.locale === "default";
 
+  console.log(
+    `Running middleware.\nLocale: ${request.nextUrl.locale}.\nUrl: ${request.nextUrl.pathname}.\nShould handle: ${shouldHandleLocale}`
+  );
   if (shouldHandleLocale) {
     const url = request.nextUrl.clone();
     url.pathname = `/de${request.nextUrl.pathname}`;
