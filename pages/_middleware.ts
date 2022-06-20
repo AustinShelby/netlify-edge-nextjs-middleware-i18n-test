0f6aa495c629b41c09887ade255a7c518f49c1f7
Austin Shelby <helloaustinshelby@gmail.com>
2022-06-20 15:30:34 +0200

    more slashes

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -23,8 +23,8 @@ export function middleware(request: NextRequest) {
     const shouldHandleLocale =
       !PUBLIC_FILE.test(request.nextUrl.pathname) &&
       !request.nextUrl.pathname.includes("/api/") &&
-      !request.nextUrl.pathname.startsWith("/en") &&
-      !request.nextUrl.pathname.startsWith("/de");
+      !request.nextUrl.pathname.startsWith("/en/") &&
+      !request.nextUrl.pathname.startsWith("/de/");
     if (shouldHandleLocale) {
       const url = request.nextUrl.clone();
       url.pathname = `/de${request.nextUrl.pathname}`;
