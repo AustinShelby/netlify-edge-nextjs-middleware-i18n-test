f8a36ea5c712e22a35ec7a967212226a3faabba8
Austin Shelby <helloaustinshelby@gmail.com>
2022-06-20 15:16:25 +0200

    omega

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,26 +3,34 @@ import { NextRequest, NextResponse } from "next/server";
 const PUBLIC_FILE = /\.(.*)$/;
 
 export function middleware(request: NextRequest) {
-  console.log(request.nextUrl);
-  // console.log(request.nextUrl.);
-  const shouldHandleLocale =
-    !PUBLIC_FILE.test(request.nextUrl.pathname) &&
-    !request.nextUrl.pathname.includes("/api/") &&
-    !request.nextUrl.pathname.startsWith("/en") &&
-    !request.nextUrl.pathname.startsWith("/de");
-  // (request.nextUrl.locale === "default" || request.nextUrl.locale === "");
-
   console.log(
-    `Running middleware.\nLocale: ${request.nextUrl.locale}.\nUrl: ${request.nextUrl.pathname}.\nShould handle: ${shouldHandleLocale}`
+    `Running middleware.\nLocale: ${request.nextUrl.locale}.\nUrl: ${request.nextUrl.pathname}`
   );
-  if (shouldHandleLocale) {
-    const url = request.nextUrl.clone();
-    const mm = `/de${request.nextUrl.pathname}`;
-    console.log(mm);
-    // url.pathname = mm;
-    url.locale = "de";
-    return NextResponse.redirect(url);
-  }
+  if (process.env.NODE_ENV === "development") {
+    const shouldHandleLocale =
+      !PUBLIC_FILE.test(request.nextUrl.pathname) &&
+      !request.nextUrl.pathname.includes("/api/") &&
+      request.nextUrl.locale === "default";
 
-  return undefined;
+    if (shouldHandleLocale) {
+      const url = request.nextUrl.clone();
+      url.pathname = `/de${request.nextUrl.pathname}`;
+      return NextResponse.redirect(url);
+    }
+
+    return undefined;
+  } else {
+    const shouldHandleLocale =
+      !PUBLIC_FILE.test(request.nextUrl.pathname) &&
+      !request.nextUrl.pathname.includes("/api/") &&
+      !request.nextUrl.pathname.startsWith("/en") &&
+      !request.nextUrl.pathname.startsWith("/de");
+    if (shouldHandleLocale) {
+      const url = request.nextUrl.clone();
+      url.pathname = `/de${request.nextUrl.pathname}`;
+      return NextResponse.redirect(url);
+    }
+
+    return undefined;
+  }
 }
